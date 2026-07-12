7f5db694f5553069b01be7b8a366d3d24c10531f
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-12 23:54:38 +0530

    Set connection between postgres Database and project

---
+++ Backend/database/connection.py
@@ -0,0 +1,19 @@
+import psycopg as pg
+import streamlit as st
+
+def connect_db():
+    try:
+        connection=pg.connect(
+            host=st.secrets["DB_HOST"],
+            dbname=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            port=st.secrets["DB_PORT"])
+
+        return connection
+    
+    except Exception as e:
+        return e
+    
+def close_db(connection):
+        connection.close()
