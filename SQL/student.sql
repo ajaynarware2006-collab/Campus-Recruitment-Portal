58ca029c1c9008892382c12af6fa511c87704933
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-12 17:57:10 +0530

    Creating a student profiles table

---
+++ SQL/student.sql
@@ -0,0 +1,25 @@
+-- Stores the personal profile information of student
+
+-- used in 
+-- student dashboard
+-- job application
+-- profile page
+
+-- relation
+-- 1 User and ! student profile
+
+CREATE TABLE IF NOT EXISTS student_profiles(
+    profile_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(254) NOT NULL,
+    encrollment_no VARCHAR(15) UNIQUE NOT NULL,
+    cgpa NUMERIC(3,2),
+    profile_img_path TEXT,
+    user_id INTEGER NOT NULL UNIQUE,
+    branch VARCHAR(7) NOT NULL,
+    semester INTEGER NOT NULL,
+    contact  VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id)
+    REFERENCES users(user_id)
+)
