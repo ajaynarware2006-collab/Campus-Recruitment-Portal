5c1d176a32a970d2f2a7c21ec420cef351c085b5
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-19 12:13:31 +0530

    Start the project again from scratch

--- Backend/admin/dashboard.py
+++
@@ -1,9 +0,0 @@
-from Backend.database.connection import connect_db
-from Backend.database.dashboard_queries import admin_dashboard
-
-
-def dashboard():
-
-    return admin_dashboard(
-        connect_db
-    )
--- SQL/authentication.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    email VARCHAR(254) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    user_role TEXT NOT NULL check (user_role IN ('Student','Admim','Recruiter')),
-    account_status TEXT NOT NULL CHECK (account_status IN ('Active','Inactive','Suspended')),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
